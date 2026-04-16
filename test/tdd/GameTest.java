b9b6156d75a7d4b2fcae3d88806815a2becdffb1
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2026-04-16 17:04:42 +0200

    init classes and tests

---
+++ test/tdd/GameTest.java
@@ -0,0 +1,21 @@
+package tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+	Game game;
+	
+	@BeforeEach
+	void init() {
+		game = new Game();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
